473f5f5b763a1544b3872ef05f45c04b4f9f88b3
fanixk <katsimpas.f@gmail.com>
2015-02-14 06:52:17 +0200

    add js seed file (call with mongo < seed.js)

---
+++ seed.js
@@ -0,0 +1,51 @@
+use geosn;
+
+db.sm.drop();
+db.gm.drop();
+
+for (var i = 1; i <= 200; i++) {
+  var friends_list = [];
+  switch (i) {
+    case 1:
+      friends_list = [i + 1];
+      break;
+    case 200:
+      friends_list = [i - 1];
+      break;
+    default:
+      friends_list = [i + 1, i - 1];
+  }
+  
+  db.sm.insert({
+    "userid": i,
+    "username": "User" + i,
+    "friends_list": friends_list
+  });
+}
+
+for (i = 1; i <= 200; i++) {
+  var random_long = Math.random() * (180 + 180) - 180;
+  var random_lat = Math.random() * (90 + 90) - 90;
+
+  db.gm.insert({
+    "userid": i,
+    "location": {
+      "type": "Point",
+      "coordinates": [random_long, random_lat]
+    }
+  });
+}
+
+db.sm.ensureIndex({
+  userid: true
+}, {
+  unique: true
+});
+db.gm.ensureIndex({
+  userid: true
+}, {
+  unique: true
+});
+db.gm.ensureIndex({
+  location: "2dsphere"
+});
